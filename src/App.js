2f0d19c26fbafe9d9d7e76009893617684f2f2c6
dailyceo <127726398+dailyceo@users.noreply.github.com>
2023-03-17 14:29:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -289,30 +289,12 @@ function App() {
                 color: "var(--accent)",
               }}
             >
-              Public Mint Mar 17, 13:30 UTC
+              MINT OPEN
             </s.TextDescription>
             <s.SpacerLarge />
 
 
 
-
-            <s.TextDescription
-              style={{
-                textAlign: "center",
-                fontSize: 40,
-                //fontWeight: "bold",
-                color: "var(--accent)",
-              }}
-            >
-              {truncate(
-                new Date(timeRemaining).toLocaleString("en-GB", {
-                  hour: "numeric",
-                  minute: "numeric",
-                  second: "numeric",
-                }),
-                10
-              )}
-            </s.TextDescription>
 
 
 
